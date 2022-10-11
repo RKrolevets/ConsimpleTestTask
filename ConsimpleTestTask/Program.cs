5f6e39a292aa88a7a1313e371ed00a0a02439ae2
Ruslan Krolevets <rkrolevets@gmail.com>
2022-10-11 21:45:31 +0300

    Add project files.

---
+++ ConsimpleTestTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsimpleTestTask;
+using System.Net;
+
+public class TestApp
+{
+
+    public static void Main()
+    {
+        bool isActiveCircle = true;
+        while (isActiveCircle)
+        {
+            Console.WriteLine("Press \"1\" to send request or press \"0\" to exit");
+            switch (Console.ReadLine())
+            {
+                case "0": isActiveCircle = false;
+                    break;
+                case "1": RequestToAPI.Execute();
+                    break;
+                default: Console.WriteLine("Wrong button");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ ConsimpleTestTask/RequestToAPI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace ConsimpleTestTask
+{
+    public class RequestToAPI
+    {
+        public static void Execute ()
+        {
+            Console.Clear();
+
+            var request = WebRequest.Create("http://tester.consimple.pro");
+            var response = request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string json = reader.ReadToEnd();
+
+            Deserializer deserializer = JsonSerializer.Deserialize<Deserializer>(json);
+            
+            var t = new TablePrinter("Product name", "Category name");
+            foreach (var product in deserializer.Products)
+            {
+                string productName = product.Name;
+                string categoryName = deserializer.Categories.First(x => product.CategoryId == x.Id).Name;
+                t.AddRow(productName, categoryName);
+            }
+            t.Print();
+        }
+    }
+}
